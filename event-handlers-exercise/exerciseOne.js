f3221dfeb1ddb24e6378d778f5961dff4c87606e
dragos.ifrim <dragos.ifrim@softvision.ro>
2019-07-23 12:38:18 +0300

    Finished exercise 02

--- event-handlers-exercise/exerciseOne.js
+++ event-handlers-exercise/exerciseOne.js
@@ -3,6 +3,7 @@
 const button = document.querySelector(".button");
 const buttonRed = document.querySelector(".button-red");
 const img = document.querySelector(".image");
+const body = document.querySelector("body");
 
 const showImage = () => {
   if (img.classList.contains("image-show")) {
@@ -19,6 +20,13 @@ const removeImg = () => {
   img.remove();
 };
 
+const checkForClick = e => {
+  console.log(e.target);
+  console.log(e.currentTarget);
+  alert("Something was clicked");
+};
+
 img.addEventListener("click", hideImg);
 button.addEventListener("click", showImage);
 buttonRed.addEventListener("click", removeImg);
+body.addEventListener("click", checkForClick);
